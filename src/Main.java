2c73c3af02ae0d1a2a57a63a346444d67e268c98
Kawasaki Hibiki <86777445+bikikiMK2@users.noreply.github.com>
2023-01-01 21:14:24 +0900

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main{
            double k = Math.pow(6.33,10^-4);
            double F = (k*(m1)*(m2)/r*r);
         System.out.println("F=" + F + "[N]");
+        // test text message
 
     }
 }
