50792ee4c322561eb6313625f277a0c6d57f100d
Kawasaki Hibiki <86777445+bikikiMK2@users.noreply.github.com>
2023-01-01 19:28:53 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args) {
+
+        System.out.println("磁極1は何[Wb]ですか？");
+          double m1 = new java.util.Scanner(System.in).nextDouble();
+        System.out.println("磁極2は何[Wb]ですか？");
+          double m2 = new java.util.Scanner(System.in).nextDouble();
+        System.out.println("磁極間の距離は何[m]ですか？");
+           double r = new java.util.Scanner(System.in).nextDouble();
+           double k = Math.pow(6.33,10^-4);
+           double F = (k*(m1)*(m2)/r*r);
+        System.out.println("F=" + F + "[N]");
+
+    }
+}
