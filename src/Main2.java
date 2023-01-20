fa52679b37657c5bfbe91ac9240c56ff8bd3b079
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-01-20 22:25:43 +0900

    Update Main.java
    Signed-off-by: bikikiMK2 <bikiki.bikiki.9180@gmail.com>

---
+++ src/Main2.java
@@ -0,0 +1,15 @@
+public class Main2 {
+    public static void main(String [] args){
+
+        System.out.println("電荷1は何[C]ですか？");
+        double Q1 = new java.util.Scanner(System.in).nextDouble();
+        System.out.println("磁極2は何[C]ですか？");
+        double Q2 = new java.util.Scanner(System.in).nextDouble();
+        System.out.println("電荷間の距離は何[m]ですか？");
+        double r = new java.util.Scanner(System.in).nextDouble();
+        double k = Math.pow(8.85,10^-12);
+        double F = (k*(Q1)*(Q2)/r*r);
+        System.out.println("F=" + F + "[N]");
+
+    }
+}
