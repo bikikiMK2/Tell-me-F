564b1b8aa806bcac3526aa6eb2a39c07ae6d2567
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-01-29 23:20:38 +0900

    Update between_electric.java

--- src/between_electric.java
+++ src/between_electric.java
@@ -8,7 +8,7 @@ public class between_electric {
          double Q2 = new java.util.Scanner(System.in).nextDouble();
         System.out.println("電荷間の距離は何[m]ですか？");
          double r = new java.util.Scanner(System.in).nextDouble();
-         double k = Math.pow(8.85,10^-12);
+         double k = Math.pow(8.85,10^(-12));
          double F = (k*(Q1)*(Q2)/r*r);
         System.out.println("F=" + F + "[N]");
 
