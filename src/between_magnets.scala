c51c1ebc2533d15307a7a794c080eddfe6b56b67
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-03-29 00:20:51 +0900

    Update between_magnets.scala

--- src/between_magnets.scala
+++ src/between_magnets.scala
@@ -4,12 +4,12 @@ import scala.language.postfixOps
 object between_magnets extends App {
   println("磁極間に働く電磁力[N]を求めます")
   println("磁極1は何[Wb]?")
-  private val m1:Double = new Scanner(System.in).nextDouble()
+   private val m1:Double = new Scanner(System.in).nextDouble()
   println("磁極2は何[Wb]?")
-  private val m2:Double = new Scanner(System.in).nextDouble()
+   private val m2:Double = new Scanner(System.in).nextDouble()
   println("磁極間の距離は何[m]?")
-  private val r:Double =new Scanner(System.in).nextDouble()
-  private val k:Double = Math.pow(6.33,10^(-4))
-  private val F:Double = k*m1*m2/r*r
+   private val r:Double = new Scanner(System.in).nextDouble()
+   private val k:Double = Math.pow(6.33,10^(-4))
+   private val F:Double = k*m1*m2/r*r
   println("F="+F+"[N]")
 }
