c3dcf1dea9d97b84532bfeee6caf6f691eb61ba6
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-03-29 02:38:59 +0900

    Update between_magnets_scala.scala

--- src/between_magnets_scala.scala
+++ src/between_magnets_scala.scala
@@ -1,7 +1,12 @@
+import java.util.Scanner
+
 object between_magnets_scala extends App {
-  def TellmeF_Magnets(m1:Double, m2:Double, r:Double): Unit = {
-    val k:Double = Math.pow(6.33,10^(-4))
-    m1*m2*k/Math.pow(r,2)
-  }
-  println(TellmeF_Magnets(10,10,100))
+  private def TellmeF_Magnets(m1:Double, m2:Double, r:Double) = m1*m2*Math.pow(6.33,10^(-4))/Math.pow(r,2)
+  println("m1は何[Wb]?")
+   val m1 = new Scanner(System.in).nextDouble()
+  println("m2は何[Wb]?")
+   val m2 = new Scanner(System.in).nextDouble()
+  println("m1,m2間は何[m]?")
+   val r = new Scanner(System.in).nextDouble()
+  println(TellmeF_Magnets(m1,m2,r))
 }
