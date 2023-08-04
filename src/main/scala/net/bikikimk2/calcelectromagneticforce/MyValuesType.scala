a8a1b64da9c224e34b6e2d1d32e636da5c35520b
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-08-04 11:23:21 +0900

    Update MyValuesType.scala

--- src/main/scala/net/bikikimk2/calcelectromagneticforce/MyValuesType.scala
+++ src/main/scala/net/bikikimk2/calcelectromagneticforce/MyValuesType.scala
@@ -1,4 +1,5 @@
-trait MyValuesType {
+package net.bikikimk2.calcelectromagneticforce
+trait  MyValuesType {
   val bettween_magnets_k: Double = 6.33 * Math.pow(10,-3)
   val bettween_magnets_F: Double
 }
